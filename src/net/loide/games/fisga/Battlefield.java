67810e0cb7aaf02e77421201d1203827a91a86c7
Luis Correia <buga@loide.net>
2011-06-08 23:56:48 +0100

    add new drawing elements

--- src/net/loide/games/fisga/Battlefield.java
+++ src/net/loide/games/fisga/Battlefield.java
@@ -28,12 +28,15 @@ public class Battlefield extends Activity {
 		mPaint.setStrokeWidth(12);
 
 	}
+
 	private Paint mPaint;
 
 	public class MyView extends View {
 
 		private Canvas mCanvas;
-		
+		private int fingerDown = 0;
+		private int constFixed = 42;
+
 		public MyView(Context c) {
 			super(c);
 
@@ -41,31 +44,45 @@ public class Battlefield extends Activity {
 
 		public void onCreate() {
 			Config conf = Bitmap.Config.ARGB_8888;
-			Bitmap mBitmap = Bitmap.createBitmap(480, 800, conf); 
+			Bitmap mBitmap = Bitmap.createBitmap(480, 800, conf);
 			setmCanvas(new Canvas(mBitmap));
 		}
 
 		@Override
 		protected void onDraw(Canvas canvas) {
+			
 			// desenhar topo e rodapé
 			mPaint.setColor(0xFF92C957);
 			canvas.drawRect(0, 0, 480, 80, mPaint);
 			canvas.drawRect(0, 720, 480, 800, mPaint);
 
-			// desenhar f
+			// desenhar fundo
 			mPaint.setColor(0xFFCDE3A1);
 			canvas.drawRect(0, 80, 480, 720, mPaint);
 
 			// desenhar linha
-			mPaint.setColor(0xFF888888);
+			mPaint.setColor(0xFF964514);
 			canvas.drawLine(200, mY, 280, mY, mPaint);
 
+			// desenhar linhas do elástico
 			mPaint.setColor(0xFFFF0000);
-			canvas.drawLine(0, 0, 200, mY, mPaint);
-			canvas.drawLine(480, 0, 280, mY, mPaint);
+			canvas.drawLine( 40,  40, 200, mY, mPaint);
+			canvas.drawLine(440,  40, 280, mY, mPaint);
+
+			// desenhar umas bolinhas para esconder a imperfeição das linhas
+			mPaint.setColor(0xFF964514);
+            canvas.drawCircle(200, mY, 8, mPaint);
+            canvas.drawCircle(280, mY, 8, mPaint);
+			
+            // desenhar os postes da fisga
+			mPaint.setColor(0xFF603311);
+            canvas.drawCircle( 40, 40,30, mPaint);
+            canvas.drawCircle(440, 40,30, mPaint);
+			
 		}
 
-		private float mX, mY;
+		private float mX = constFixed;
+		private float mY = constFixed;
 		private static final float TOUCH_TOLERANCE = 4;
 
 		private void touch_start(float x, float y) {
@@ -83,9 +100,9 @@ public class Battlefield extends Activity {
 		}
 
 		private void touch_up() {
-			//TODO send Y position to server
-			mX = 0;
-			mY = 0;
+			// TODO send Y position to server
+			mX = constFixed;
+			mY = constFixed;
 		}
 
 		@Override
@@ -95,16 +112,25 @@ public class Battlefield extends Activity {
 
 			switch (event.getAction()) {
 			case MotionEvent.ACTION_DOWN:
-				touch_start(x, y);
-				invalidate();
+				// só apanhar a fisga em cima
+				if (y <= 80) {
+					fingerDown = 1;
+					touch_start(x, y);
+					invalidate();
+				}
 				break;
+
 			case MotionEvent.ACTION_MOVE:
-				touch_move(x, y);
-				invalidate();
+				if (fingerDown == 1) {
+					touch_move(x, y);
+					invalidate();
+				}
 				break;
+
 			case MotionEvent.ACTION_UP:
 				touch_up();
 				invalidate();
+				fingerDown = 0;
 				break;
 			}
 			return true;
